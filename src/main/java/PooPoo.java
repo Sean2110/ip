2fe1ed23de21438faf167fbdc4d201c59fe7e54d
SeanPoon <e1155711@u.nus.edu>
2025-01-24 13:40:39 +0800

    week 2 level 0

---
+++ src/main/java/PooPoo.java
@@ -0,0 +1,10 @@
+public class PooPoo {
+    public static void main(String[] args) {
+        String Name = "PooPoo";
+        String Ask = "How can I help you?";
+        String Bye = "Bye! Hope to see you soon!";
+        System.out.println("Hello! I am " + Name);
+        System.out.println(Ask + "\n");
+        System.out.println(Bye + "\n");
+    }
+}
