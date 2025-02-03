2f467827c066b3b4995f21f8af28eac5bcbb1a5e
SeanPoon <e1155711@u.nus.edu>
2025-02-03 14:39:45 +0800

    week 3 echo level 1

--- src/main/java/PooPoo.java
+++ src/main/java/PooPoo.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class PooPoo {
     public static void main(String[] args) {
         String Name = "PooPoo";
         String Ask = "How can I help you?";
         String Bye = "Bye! Hope to see you soon!";
-        System.out.println("Hello! I am " + Name);
-        System.out.println(Ask + "\n");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hii! Nice to meet you! I'm " + Name);
+        System.out.println(Ask);
+
+        //echo level 1
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+
         System.out.println(Bye + "\n");
     }
 }
