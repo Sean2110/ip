1b32fd7496a617f66638e6f70af05753a0a11d87
SeanPoon <e1155711@u.nus.edu>
2025-02-06 01:09:03 +0800

    Level-2

--- src/main/java/PooPoo.java
+++ src/main/java/PooPoo.java
@@ -1,21 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PooPoo {
+    public static ArrayList<String> taskList = new ArrayList<>(100);
     public static void main(String[] args) {
         String Name = "PooPoo";
         String Ask = "How can I help you?";
         String Bye = "Bye! Hope to see you soon!";
+
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Hii! Nice to meet you! I'm " + Name);
         System.out.println(Ask);
 
-        //echo level 1
         while (true) {
             String command = sc.nextLine();
             if (command.equalsIgnoreCase("bye")) {
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                for (int i = 1; i <= taskList.size(); i++) {
+                    System.out.println(i + ". " + taskList.get(i - 1));
+                }
             } else {
+                taskList.add(command);
+                System.out.print("added ");
                 System.out.println(command);
             }
         }
