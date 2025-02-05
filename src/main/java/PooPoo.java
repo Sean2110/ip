17555bb19f11e0979d4c61c91e8ea1ab9029f240
SeanPoon <e1155711@u.nus.edu>
2025-02-06 01:16:43 +0800

    Level-3

--- src/main/java/PooPoo.java
+++ src/main/java/PooPoo.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PooPoo {
-    public static ArrayList<String> taskList = new ArrayList<>(100);
+    public static ArrayList<Task> taskList = new ArrayList<>(100);
     public static void main(String[] args) {
         String Name = "PooPoo";
         String Ask = "How can I help you?";
@@ -17,13 +17,23 @@ public class PooPoo {
         while (true) {
             String command = sc.nextLine();
             if (command.equalsIgnoreCase("bye")) {
+                // goodbye
                 break;
             } else if (command.equalsIgnoreCase("list")) {
+                // list all tasks
                 for (int i = 1; i <= taskList.size(); i++) {
-                    System.out.println(i + ". " + taskList.get(i - 1));
+                    System.out.println(i + ". " + taskList.get(i - 1).toString());
                 }
+            } else if (command.startsWith("mark")) {
+                // mark as done
+                String[] tokens = command.split(" ");
+                int index = Integer.parseInt(tokens[1]);
+                taskList.get(index - 1).markAsDone();
+                // sucessfully marked as done.
+                System.out.println("Thank you! I have marked it as done! Good job!");
             } else {
-                taskList.add(command);
+                // add new task
+                taskList.add(new Task(command));
                 System.out.print("added ");
                 System.out.println(command);
             }
